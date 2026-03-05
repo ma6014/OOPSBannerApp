6f5a23fa20f6c5eda434dcef7fb902da523c7be5
ma6014 <ma60142@srmist.edu.in>
2026-03-05 13:03:17 +0530

    Adding java file

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+
+/**
+ * OOPS Banner App
+ * UC1 - Print OOPS 
+ * @author Mugesh Krishna
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+    }
+}
