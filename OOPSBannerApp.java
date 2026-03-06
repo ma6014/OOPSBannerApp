381e3cff44f030029dcd030087313fa9ed7e0c89
ma6014 <ma60142@srmist.edu.in>
2026-03-06 09:29:08 +0530

    Adding file

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,8 @@
 /**
  * OOPS Banner App
- * UC5 - Render OOPS as Banner using Inline Array Initialization
+ * UC6 - Refactor Banner Logic into Functions
  * @author Mugesh Krishna
- * @version 5.0
+ * @version 6.0
  */
 
 public class OOPSBannerApp {
@@ -10,17 +10,69 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
 
         String[] banner = {
-            String.join("  ", " ***** ", " ***** ", " ***** ", " ***** "),
-            String.join("  ", "*     *", "*     *", "*     *", "*     *"),
-            String.join("  ", "*     *", "*     *", "*     *", "*     *"),
-            String.join("  ", "*     *", " ***** ", "*     *", " ***** "),
-            String.join("  ", "*     *", "*      ", "*     *", "     * "),
-            String.join("  ", "*     *", "*      ", "*     *", "     * "),
-            String.join("  ", " ***** ", "*      ", " ***** ", " ***** ")
+            buildLine(0),
+            buildLine(1),
+            buildLine(2),
+            buildLine(3),
+            buildLine(4),
+            buildLine(5),
+            buildLine(6)
         };
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Helper method to assemble full banner row
+    public static String buildLine(int row) {
+        return String.join("   ",
+                getOPattern(row),
+                getOPattern(row),
+                getPPattern(row),
+                getSPattern(row)
+        );
+    }
+
+    // O Pattern
+    public static String getOPattern(int row) {
+        String[] O = {
+            " *****  ",
+            "*     * ",
+            "*     * ",
+            "*     * ",
+            "*     * ",
+            "*     * ",
+            " *****  "
+        };
+        return O[row];
+    }
+
+    // P Pattern
+    public static String getPPattern(int row) {
+        String[] P = {
+            " ****** ",
+            "*     * ",
+            "*     * ",
+            " ****** ",
+            "*        ",
+            "*        ",
+            "*        "
+        };
+        return P[row];
+    }
+
+    // S Pattern
+    public static String getSPattern(int row) {
+        String[] S = {
+            "  *****  ",
+            " *     * ",
+            " *        ",
+            "  *****  ",
+            "       * ",
+            " *     * ",
+            "  *****  "
+        };
+        return S[row];
     }
 }
